fc213b4141ae9d33619859ea5bba7ebf24691666
Suraj Somkuwar <sssomkuwar2@gmail.com>
2026-06-12 21:04:46 +0530

    Added insecure S3 bucket configuration for security analysis

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "securestack-public-bucket"
+}
 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
