ed0f0cfb6e9ef9d4cbf3f247b6b129b5f06ff210
Suraj Somkuwar <sssomkuwar2@gmail.com>
2026-06-15 22:04:00 +0530

    Add secret scanning test

--- sample-app/app.py
+++ sample-app/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
+GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+
 @app.route("/")
 def home():
     return "Hello DevSecOps"
