d044fab458e7c5c9517deb6c268265778bbf1383
Suraj Somkuwar <sssomkuwar2@gmail.com>
2026-06-15 21:47:04 +0530

    Add AWS secret test pattern

--- sample-app/app.py
+++ sample-app/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-AWS_SECRET_ACCESS_KEY = "my-super-secret-key"
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 @app.route("/")
 def home():
